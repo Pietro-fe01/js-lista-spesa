4ee48812da0d28bdee5454bb74e3fd262aec346e
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-27 14:19:28 +0200

    Algoritmo realizzato

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 /* Consegna:
 Data una lista della spesa, stampare 
-sulla pagina (anche brutalmente, basta che si vedano) 
-gli elementi della lista individualmente con un ciclo while. */
+sulla pagina (anche brutalmente, basta che
+si vedano) 
+gli elementi della lista individualmente 
+con un ciclo while. */
+
+const unorderedList = document.getElementById("unordered-list");
+const shoppingList = [
+    'Latte',
+    'Pasta',
+    'Biscotti',
+    'Insalata',
+    'Toast',
+    'Nutella',
+    'Burro',
+    'Farina'
+];
+
+let i = 0;
+while (i < shoppingList.length){
+    const listItem = document.createElement("li");
+    listItem.innerHTML = shoppingList[i];
+    unorderedList.append(listItem);
+    i++;
+};
